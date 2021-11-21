0fe7747d577494bc61998a961083485d8c83d53d
Ismael Bermudez <ismaelberfer@gmail.com>
2021-11-21 18:23:35 +0100

    test: add test to product was duplicated EH

---
+++ internal/report/application/event-handlers/product_was_duplicated_test.go
@@ -0,0 +1,27 @@
+package event_handlers
+
+import (
+	"deporvillage-feeder-backend/internal/inventory/domain/events"
+	"deporvillage-feeder-backend/internal/report/domain"
+	"deporvillage-feeder-backend/internal/report/infrastructure"
+	"testing"
+)
+
+func setupProductWasDuplicated(rm map[string]domain.Report) (ProductWasDuplicatedApplicationService, infrastructure.ReportRepository) {
+	repository := infrastructure.NewReportRepository(rm)
+	return CreateProductWasDuplicatedApplicationService(repository), repository
+}
+
+func TestProductWasDuplicated(t *testing.T) {
+	// arrange
+	rm := make(map[string]domain.Report)
+	as, r := setupProductWasDuplicated(rm)
+
+	as.Execute(events.ProductWasDuplicated{ProductSKU: "ABCD-1234"})
+
+	re, _ := r.Find(domain.ReportId{Value: "1"})
+
+	if re.CountProductsDuplicated != 1 {
+		t.Errorf("error when running application servirce")
+	}
+}
