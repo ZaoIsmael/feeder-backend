3381b502740509e0c2ec4d805b98978ca1c28ae6
Ismael Bermudez <ismaelberfer@gmail.com>
2021-11-22 00:31:39 +0100

    chore: method update to pointer

--- pkg/domain/aggregate_root.go
+++ pkg/domain/aggregate_root.go
@@ -1,16 +1,18 @@
 package domain
 
 type AggregateRoot struct {
-	domainEvents []EventDomain
+	DomainEvents []EventDomain
 }
 
-func (a AggregateRoot) RegisterEvent(e EventDomain) {
-	a.domainEvents = append(a.domainEvents, e)
+func (a *AggregateRoot) RegisterEvent(e EventDomain) {
+	a.DomainEvents = append(a.DomainEvents, e)
 }
 
-func (a AggregateRoot) Pull() []EventDomain {
-	events := a.domainEvents
+func (a *AggregateRoot) Pull() []EventDomain {
+	events := make([]EventDomain, len(a.DomainEvents))
+	copy(events, a.DomainEvents)
 
-	a.domainEvents = nil
+	a.DomainEvents = nil
+
 	return events
 }
