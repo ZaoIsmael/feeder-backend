5116b6fc55148212c0506a49f72d85b548b278da
Ismael Bermudez <ismaelberfer@gmail.com>
2021-11-22 17:52:14 +0100

    chore: rename import

--- cmd/feeder-service/main.go
+++ cmd/feeder-service/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	server2 "deporvillage-feeder-backend/cmd/feeder-service/src/server"
+	"deporvillage-feeder-backend/cmd/feeder-service/src/server"
 	"fmt"
 	"log"
 )
 
 func main() {
-	app, err := server2.Boostrap()
+	app, err := server.Boostrap()
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	srv, err := server2.CreateServer(app.Service)
+	srv, err := server.CreateServer(app.Service)
 
 	if err != nil {
 		log.Fatal(err)
